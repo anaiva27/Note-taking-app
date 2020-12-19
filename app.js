54147011c00f94e27eb87d8fefe46f8c657425cf
Anastasia <nastia.iv.91@gmail.com>
2020-12-19 14:25:41 -0800

    heroku

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // require and set up express
 const express = require("express")
 const app = express();
+const path = require("path");
 
 // set an initial port
 let PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"/public")));
 
 // set up routes
 require("./routes/apiRoutes")(app)
